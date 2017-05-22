1a29bfebf0155472fc3beecb15f03fc54d7ff69d
Mathias Ewald <mewald@evoila.de>
2017-05-22 15:49:30 +0200

    Fix: Path to jq

--- src/scripts/centos/3_install-targets.sh
+++ src/scripts/centos/3_install-targets.sh
@@ -14,15 +14,15 @@ truncate -s 0 /etc/prometheus/tgroups/$JOB_NAME.yaml
 echo "Cleared file /etc/prometheus/tgroups/$JOB_NAME.yaml"
 
 # Write targets
-LENGTH=$(echo ${ADDRESSES} | /usr/bin/jq 'length')
+LENGTH=$(echo ${ADDRESSES} | jq 'length')
 LAST_INDEX=$((LENGTH-1))
 
 echo "LENGTH: $LENGTH"
 echo "LAST_INDEX: $LAST_INDEX"
 
 for I in `seq 0 $LAST_INDEX`; do
-  ADDR=$(echo $ADDRESSES | /usr/bin/jq -r ".[$I]" )
-  NAME=$(echo $NAMES | /usr/bin/jq -r ".[$I]" )
+  ADDR=$(echo $ADDRESSES | jq -r ".[$I]" )
+  NAME=$(echo $NAMES | jq -r ".[$I]" )
   cat <<EOF >> /etc/prometheus/tgroups/$JOB_NAME.yaml
 - targets: ["$ADDR"]
   labels:
