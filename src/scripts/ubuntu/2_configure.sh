ecdafc7289d8b1c077667988481908be7ba515ea
Mathias Ewald <mewald@evoila.de>
2017-07-08 22:42:51 +0200

    Feature: Templated Configuration File
    Use template instead of embedded prometheus.yaml
    
    Add parameter config_template to provide configuration template for
    prometheus.yaml
    
    Add parameter template_vars to pass map used to interpolate variables in
    config_template

--- src/scripts/ubuntu/2_configure.sh
+++ src/scripts/ubuntu/2_configure.sh
@@ -1,22 +1,17 @@
-#!/bin/bash
+#!/bin/bash -x
 
-JOB_DEFINITIONS=${JOB_DEFINITIONS}
+CONFIG_TEMPLATE="${CONFIG_TEMPLATE}"
+TEMPLATE_VARS=${TEMPLATE_VARS:-'{}'}
 
 PRIMARY_NIC=$(route -n | grep -E "^0.0.0.0" | awk '{print $8}')
 PRIMARY_ADDR=$(ip addr show dev $PRIMARY_NIC | grep -E "inet " | awk '{print $2}' | cut -d '/' -f1)
 
-cat <<EOF > /etc/prometheus/prometheus.yml
-global:
-  scrape_interval:     10s
-  evaluation_interval: 10s
-scrape_configs:
-  - job_name: 'prometheus'
-    scrape_interval: 5s
-    static_configs:
-      - targets: ['$PRIMARY_ADDR:9090']
-  - job_name: 'dummy'
-    file_sd_configs:
-      - files: ['/etc/prometheus/tgroups/*.yaml']
-EOF
+for KEY in `echo $TEMPLATE_VARS | jq -r '. as $in| keys[]'`; do
+  VAL=$(echo $TEMPLATE_VARS | jq -r .$KEY)
+  export $KEY=$VAL
+done
+
+CONFIG=$(eval "echo -e \"$CONFIG_TEMPLATE\"")
+echo "$CONFIG" > /etc/prometheus/prometheus.yml
 
 service prometheus restart
