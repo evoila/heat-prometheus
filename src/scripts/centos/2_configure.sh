6f1257b351fc605f13a0070441fcff1a9aa1cbde
Mathias Ewald <mewald@evoila.de>
2017-07-10 08:28:45 +0200

    Fix: Primary address detection with multiple addresses per NIC

--- src/scripts/centos/2_configure.sh
+++ src/scripts/centos/2_configure.sh
@@ -4,7 +4,7 @@ CONFIG_TEMPLATE="${CONFIG_TEMPLATE}"
 TEMPLATE_VARS=${TEMPLATE_VARS:-'{}'}
 
 PRIMARY_NIC=$(route -n | grep -E "^0.0.0.0" | awk '{print $8}')
-PRIMARY_ADDR=$(ip addr show dev $PRIMARY_NIC | grep -E "inet " | awk '{print $2}' | cut -d '/' -f1)
+PRIMARY_ADDR=$(ip addr show dev $PRIMARY_NIC | grep -E "inet " | head -n1 | awk '{print $2}' | cut -d '/' -f1)
 
 for KEY in `echo $TEMPLATE_VARS | jq -r '. as $in| keys[]'`; do
   VAL=$(echo $TEMPLATE_VARS | jq -r .$KEY)
